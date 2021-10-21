e88be14fbfd7d0a6febcdadb5f2258829d5c8b64
Anatolii <anatolii.neveselyi@gmail.com>
2021-10-21 16:57:51 +0300

    create calc

---
+++ Simple calc.py
@@ -0,0 +1,18 @@
+# Simple calculator 
+opr = input ("What operation are you choosesing ? ( + , - , * , /): ")
+a = float( input("Type first number: ") )
+b = float( input("Type second number: ") )
+
+if opr == "+":
+    c = a + b 
+    print("Result: " + str(c))
+elif opr == "-":
+    c = a - b 
+    print("Result: " + c) 
+elif opr == "*":
+    c = a * b 
+    print("Result: " + c)
+elif opr == "/":
+    c = a / b 
+    print("Result: " + c)        
+
