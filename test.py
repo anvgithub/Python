c4b3a4ee9eb5da5c9c82a644fc387df5b7301f87
Anatolii <anatolii.neveselyi@gmail.com>
2021-10-21 16:18:24 +0300

    test  remote repo

---
+++ test.py
@@ -0,0 +1,9 @@
+#name = input( "Enter your name:")                         
+#age = input("Enter you age:")                             
+#print("Hello, " + name + " !")                            
+#print("You is " + age + " years old !!!")                 
+                                                           
+a = -5                                                     
+b = 10                                                     
+c = b - a                                                  
+print(c)                                                   
