71ef1707af078cdd581949e1c707b1f6a5d00e36
Anatolii <anatolii.neveselyi@gmail.com>
2021-10-22 17:55:27 +0300

    test

--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-#name = input( "Enter your name:")                         
-#age = input("Enter you age:")                             
-#print("Hello, " + name + " !")                            
-#print("You is " + age + " years old !!!")                 
 import math                                                          
 a = -5.77                                                     
 b = 10                                                     
